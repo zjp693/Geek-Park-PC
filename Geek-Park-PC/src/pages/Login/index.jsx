e7eaa2e3bf161941aac852b3d4536b0d53d27c3b
zjp693 <942680978@qq.com>
2022-04-22 19:39:04 +0800

    项目初始化

---
+++ Geek-Park-PC/src/pages/Login/index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import logo from "@/";
+
+import "./index.scss";
+
+const Login = () => {
+  return (
+    <div className="login">
+      <div className="login-container">
+        <img className="login-logo" src={logo} alt="" />
+        {/* 登录表单 */}
+      </div>
+    </div>
+  );
+};
+
+export default Login;
