fba6acf23e6b8590f5ad18ca5c6bca9b5b6f62b8
zjp693 <942680978@qq.com>
2022-04-28 09:30:03 +0800

    update:删除不必要的引入

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { BrowserRouter } from "react-router-dom";
-import RouterWaiter from "react-router-waiter";
 import AppRoutes from "./routes/index";
-import onRouteBefore from "./routes/onRouteBefore";
 // import 自定义模块名称 from ''
 
 function App() {
